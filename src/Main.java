d31cdc05564dbb7f2997d9ddb90fc2faa767d4bc
Jean Caetano <caetanojean4@gmail.com>
2025-04-03 16:17:59 -0300

    Criando e emplementando métodos para criação de mensagens, como nome do villager, a cor desse nome, o que ele diz (falta implementar a cor da mensagem)

--- src/Main.java
+++ src/Main.java
@@ -59,16 +59,14 @@ class SummonVillager {
 // Classe pra tratar de métodos usados pra reiniciar processos
 class Reinicio {
     int vReiniciar;
-    boolean bTrava2 = true;
     int vConfirm;
-
+    Scanner input = new Scanner(System.in);
     // Método para ler a variavel vReiniciar
     public int lerREINICIAR() {
         return vReiniciar;
     }
     // Método para gravar na variavel de vReiniciar
     public void setREINICIAR() {
-        Scanner input = new Scanner(System.in);
         while (true) {
             System.out.println("Você quer refazer o processo? (0 = Sim, 1 = Não)");
             this.vConfirm = input.nextInt();
@@ -84,6 +82,13 @@ class Reinicio {
 class Objeto {
     Scanner input = new Scanner(System.in);
     String vObjeto; 
+    String vNomeCodigo;
+    String vNome;
+    int vCorNomeInt;
+    String vCorNomeString;
+    int vCorMsgInt;
+    String vCorMsgString;
+    String vMsg;
     // Método para gravar o nome do Objeto
     public void setObjeto() {
         System.out.print("Declare o Objeto: ");
@@ -93,6 +98,63 @@ class Objeto {
     // Método para ler o nome do Objeto
     public String lerObjeto() {
         return vObjeto;
+    }
+    // Método para REFERENCIAR o Villager spawnado no código e definir o nome escrito em tela
+    public void setNomeCodigo() {
+        System.out.print("Digite o nome a ser REFERENCIADO para esse Villager no código: ");
+        this.vNomeCodigo = input.nextLine();
+        System.out.printf("Nome salvo no código como: %s%s", vNomeCodigo, "\n");
+        System.out.print("Digite o nome que irá APARECER no TEXTO para esse Villager: ");
+        this.vNome = input.nextLine();
+        System.out.printf("Nome que será exibido: %s%s", vNome, "\n");
+    }
+    // Método para ler o nome de REFERENCIA no CODIGO
+    public String lerNomeCodigo() {
+        return vNomeCodigo;
+    }
+    // Método para ler o nome a ser EXIBIDO
+    public String lerNome() {
+        return vNome;
+    }
+    // Método para definir a cor do NOME EXIBIDO
+    public void setCorNome() {
+        System.out.print("Qual cor o nome de exibição deve ter?\n(0 = Verde, 1 = Dourado, 2 = Vermelho, 3 = Azul, 4 = Roxo Escuro) \n");
+        this.vCorNomeInt = input.nextInt();
+        switch (vCorNomeInt) {
+            case 0:
+                vCorNomeString = "green"; // Verde
+                break;
+            case 1:
+                vCorNomeString = "gold"; // Dourado
+                break;
+            case 2:
+                vCorNomeString = "red"; // Vermelho
+                break;
+            case 3:
+                vCorNomeString = "blue"; // Azul
+                break;
+            case 4:
+                vCorNomeString = "dark_purple"; // Roxo Escuro
+                break;
+            default:
+                vCorNomeString = "white"; // branco
+                break;
+        }
+        System.out.printf("Cor selecionada: %s%s", vCorNomeString, "\n");  
+    }
+    //Método para ler a cor do nome EXIBIDO
+    public String lerCorNome() {
+        return vCorNomeString;
+    }
+    // Método para gravar a Mensagem exibida
+    public void setMsg() {
+        System.out.println("O que o Villager em questão vai dizer?");
+        this.vMsg = input.nextLine();
+        System.out.printf("O Villager vai dizer:\n%s", vMsg);
+    }
+    // Método para ler a Mensagem exibida
+    public String lerMsg() {
+        return vMsg;
     }
 }
 public class Main {
@@ -107,13 +169,26 @@ public class Main {
             System.out.println("Copie o codigo e cole o codigo abaixo para gerar um Villager com as customizações acima: \n");
             if (villager.getAi().isEmpty()) { // Menssagem para NPC com IA
                 System.out.println("/summon minecraft:villager ~ ~ ~ {Rotation:" + villager.getDirecao() + "}\n");
-                Objeto nomeObjeto = new Objeto();
+                // Separando as fazes de criação manualmente :)
+                System.out.println("Passando para criação de mensagem -->");
                 // Criando um objeto para referenciar
+                Objeto nomeObjeto = new Objeto();
+                // Criando um objeto para tratar do nome em CÓDIGO e EXIBIÇÂO
+                Objeto nomeVillager = new Objeto();
+                // Criando um objeto para tratar da mensagem EXIBIDA
+                Objeto mensagemVillager = new Objeto();
                 System.out.println("Primeiro você precisa criar um OBJETO para se referenciar a esse villager: ");
                 nomeObjeto.setObjeto();
                 // Imprimindo o codigo para criar o objeto interativo
                 System.out.println("Copie e cole o codigo abaixo para começar a criação de dialogo: \n");
                 System.out.println("/scoreboard objectives add " + nomeObjeto.lerObjeto() + " minecraft.custom:minecraft.talked_to_villager" + "\n");
+                // Definindo o nome no código e de exibição do villager
+                nomeVillager.setNomeCodigo(); 
+                // Definindo a cor do nome
+                nomeVillager.setCorNome();
+                // Definindo a Mensagem 
+                mensagemVillager.setMsg();
+
                 // Chamando verificação de reiniciar
                 reiniciar.setREINICIAR();
                 if (reiniciar.lerREINICIAR() == 1) {
@@ -122,13 +197,25 @@ public class Main {
                 }
             } else {    // Menssagem para NPC sem IA
                 System.out.println("/summon minecraft:villager ~ ~ ~ {" + villager.getAi() + ",Rotation:" + villager.getDirecao() + "}\n");
-                Objeto nomeObjeto = new Objeto();
+                // Separando as fazes de criação manualmente :)
+                System.out.println("Passando para criação de mensagem -->");
                 // Criando um objeto para referenciar
+                Objeto nomeObjeto = new Objeto();
+                // Criando um objeto para tratar do nome em CÓDIGO e EXIBIÇÂO
+                Objeto nomeVillager = new Objeto();
+                // Criando um objeto para tratar da mensagem EXIBIDA
+                Objeto mensagemVillager = new Objeto();
                 System.out.println("Primeiro você precisa criar um OBJETO para se referenciar a esse villager: ");
                 nomeObjeto.setObjeto();
                 // Imprimindo o codigo para criar o objeto interativo
                 System.out.println("Copie e cole o codigo abaixo para começar a criação de dialogo: \n");
                 System.out.println("/scoreboard objectives add " + nomeObjeto.lerObjeto() + " minecraft.custom:minecraft.talked_to_villager" + "\n");
+                // Definindo o nome no código e de exibição do villager
+                nomeVillager.setNomeCodigo(); 
+                // Definindo a cor do nome
+                nomeVillager.setCorNome();
+                // Definindo a Mensagem 
+                mensagemVillager.setMsg();
 
                 // Chamando verificação de reiniciar
                 reiniciar.setREINICIAR();
@@ -140,12 +227,23 @@ public class Main {
             // Verifica de onde começa o código
         } else if (villager.lerSpawn() == 1) {
             Objeto nomeObjeto = new Objeto();
+            Objeto nomeVillager = new Objeto();
+            Objeto mensagemVillager = new Objeto();
+            // Separando as fazes de criação manualmente :)
+            System.out.println("Passando para criação de mensagem -->");
             // Criando um objeto para referenciar
             System.out.println("Primeiro você precisa criar um OBJETO para se referenciar a esse villager: ");
             nomeObjeto.setObjeto();
             // Imprimindo o codigo para criar o objeto interativo
             System.out.println("Copie e cole o codigo abaixo para começar a criação de dialogo: \n");
             System.out.println("/scoreboard objectives add " + nomeObjeto.lerObjeto() + " minecraft.custom:minecraft.talked_to_villager" + "\n");
+            // Definindo o nome no código e de exibição do villager
+            nomeVillager.setNomeCodigo(); 
+            // Definindo a cor do nome
+            nomeVillager.setCorNome();
+            // Definindo a Mensagem 
+            mensagemVillager.setMsg();
+
             // Chamando verificação de reiniciar
             reiniciar.setREINICIAR();
             if (reiniciar.lerREINICIAR() == 1) {
